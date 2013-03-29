0f50bb03b3fbaef8f62eb8eb37a5e2332daca357
registrobr <epp-piloto@registro.br>
2013-03-29 08:48:49 -0300

    Config instructions now part of HOWTO

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-whmcsdir="/var/www/whmcs"
-crontabfile="/var/spool/cron/crontabs/root"
-whmcswebdomain="whmcs.in.registro.br"
+whmcsdir=$1
+crontabfile=$2
+whmcswebdomain=$3
 
-#find the whoisservers.php
+: ${whmcsdir:="/var/www/whmcs"}
+: ${crontabfile:="/var/spool/cron/crontabs/root"}
+: ${whmcswebdomain:=$(hostname --long)}
+
+
+
+#location of whoisservers.php
 oldlist=$whmcsdir/includes/whoisservers.php
 
-#replace domain in the listservers.txt
+#replace .br domains with contents of listservers.txt
 list="listservers.txt"
 domain="localhost"
 sed -e "s,$domain,$whmcswebdomain,g" listservers.txt > listserversnew.txt
@@ -15,7 +21,7 @@ sed -e "s,$domain,$whmcswebdomain,g" listservers.txt > listserversnew.txt
 mv listserversnew.txt listservers.txt
 
 #remove old .br entries
-awk '!/.br/' $oldlist > tmplist.php
+awk '!/.br|/' $oldlist > tmplist.php
 
 #add new .br entries
 cat tmplist.php $list > newlist.php
@@ -30,10 +36,13 @@ rm newlist.php
 cp Avail.php  brdomaincheck.php $whmcsdir/
 cp -r registrobr  $whmcsdir/modules/registrars/
 
-#colocar poll na crontab
-sync="$whmcsdir/modules/registrars/registrobr/registrobrpoll.php"
+#add poll process to crontab
+poll="$whmcsdir/modules/registrars/registrobr/registrobrpoll.php"
 path="PATH"
-sed -e "s,$path,$sync,g" crontab.txt > crontabnew.txt
+sed -e "s,$path,$poll,g" crontab.txt > crontabnew.txt
+
+#if domain sync is available, add it to crontab as well
+
 
 cat $crontabfile crontabnew.txt > crontabtmp.txt
 cp crontabtmp.txt $crontabfile
